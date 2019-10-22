700b555392d4e34acb7b6b0c2f6fd3b02aa24861
Rad <chihaby@gmail.com>
2019-10-22 09:30:02 -0700

    hides keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyB172PE0IKZfUK4KAeo1xC9YQBvG0kO3Sc",
-  authDomain: "lunch-rush-4d021.firebaseapp.com",
-  databaseURL: "https://lunch-rush-4d021.firebaseio.com",
-  projectId: "lunch-rush-4d021",
-  storageBucket: "lunch-rush-4d021.appspot.com",
-  messagingSenderId: "985586640739",
-  appId: "1:985586640739:web:881af6c5276a70c0889556",
-  measurementId: "G-K8LLX69QW7"
+
 };
 
 firebase.initializeApp(config);
