f0716163bf32e070b8e0330200c5f494460345a4
Rad <chihaby@gmail.com>
2019-10-22 20:22:21 -0700

    adds vote

--- src/Application.js
+++ src/Application.js
@@ -1,57 +1,58 @@
-import React, { Component } from 'react';
-import { auth, database } from './firebase';
-import CurrentUser from './CurrentUser';
-import SignIn from './SignIn';
-import NewRestaurant from './NewRestaurant';
-import Restaurants from './Restaurants';
-import './Application.css';
+import React, { Component } from "react";
+import { auth, database } from "./firebase";
+import CurrentUser from "./CurrentUser";
+import SignIn from "./SignIn";
+import NewRestaurant from "./NewRestaurant";
+import Restaurants from "./Restaurants";
+import "./Application.css";
 
-import map from 'lodash/map';
+import map from "lodash/map";
 
 class Application extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      CurrentUser: null,
-      restaurants: null
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            CurrentUser: null,
+            restaurants: null
+        };
 
-    this.restaurantRef = database.ref('/restaurants');
-  }
+        this.restaurantRef = database.ref("/restaurants");
+    }
 
-  componentDidMount () {
-    // currentUser (lower case c ) is just a random name. It can be any name
-    auth.onAuthStateChanged((currentUser) => {
-      this.setState({currentUser});
+    componentDidMount() {
+        // currentUser (lower case c ) is just a random name. It can be any name
+        auth.onAuthStateChanged(currentUser => {
+            this.setState({ currentUser });
 
-      this.restaurantRef.on('value', (snapshot) => {
-        this.setState({restaurants: snapshot.val() });
-      });
-    });
-  }
+            this.restaurantRef.on("value", snapshot => {
+                this.setState({ restaurants: snapshot.val() });
+            });
+        });
+    }
 
-
-  render() { 
-    const { currentUser, restaurants } = this.state;
-    return (
-      <div className="Application">
-        <header className="Application--header">
-          <h1>Lunch Rush</h1>
-        </header>
-        <div>
-          {!currentUser && <SignIn />}
-          {
-            currentUser && 
-              <div>
-                <NewRestaurant />
-                <Restaurants restaurants={restaurants} />
-                <CurrentUser user={currentUser} />
-              </div>
-            }
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const { currentUser, restaurants } = this.state;
+        return (
+            <div className="Application">
+                <header className="Application--header">
+                    <h1>Lunch Rush</h1>
+                </header>
+                <div>
+                    {!currentUser && <SignIn />}
+                    {currentUser && (
+                        <div>
+                            <NewRestaurant />
+                            <Restaurants
+                                restaurants={restaurants}
+                                user={currentUser}
+                            />
+                            <CurrentUser user={currentUser} />
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Application;
